442191408a55a621618862214a4c606881eb14e3
JuanModesto <ingjuanmodesto@gmail.com>
2023-05-13 20:54:24 -0600

    Config del folder de cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   e2e: {
+    supportFile: false,
+    specPattern: './cypress/integration/*.js'
   },
   video: false
 })
